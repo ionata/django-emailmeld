32fbe9ff09b2a7fd09d5e2d084dd0f32c7b151e8
Chris Darko <chris@ionata.com.au>
2016-07-07 12:27:12 +1000

    v0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-emailmeld',
-    version='0.0.5',
+    version='0.0.6',
     description='Django Email Templater.',
     long_description = open( 'README.md', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Thomas',
