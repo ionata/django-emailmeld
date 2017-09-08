28326933d22957f8737ab8a9564daa9cbfca6d06
Chris Darko <chris@ionata.com.au>
2017-09-08 10:07:26 +1000

    Modernise setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,40 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, print_function
+
+import io
+
 from setuptools import setup, find_packages
+
+
+def _read(filename):
+    with io.open(filename, encoding='utf-8') as handle:
+        return handle.read()
+
 
 setup(
     name='django-emailmeld',
     version='0.0.6',
     description='Django Email Templater.',
-    long_description = open( 'README.md', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
-    author='Thomas',
+    long_description=_read('README.md') + _read('AUTHORS.rst') + _read('CHANGELOG.rst'),
+    author='Ionata Digital',
     author_email='spam@ionata.com.au',
-    url='http://github.com/ionata/django-emailmeld',
+    url='https://github.com/ionata/django-emailmeld',
     packages = find_packages(exclude=['project',]),
-    install_requires = [
-        'markdown'
-    ],
+    install_requires=['markdown'],
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python",
-        "License :: OSI Approved :: BSD License",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         "Development Status :: 4 - Beta",
-        "Operating System :: OS Independent",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
-    zip_safe=False,
 )
